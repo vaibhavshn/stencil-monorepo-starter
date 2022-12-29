a2799e5c8c05d09622e5247e8f5e503295675f49
Vaibhav Shinde <vaibhavshn.in@gmail.com>
2022-12-29 22:09:32 +0530

    feat: added react-library

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,8 +1,15 @@
 import { Config } from '@stencil/core';
 
+import { reactOutputTarget as react } from '@stencil/react-output-target';
+
 export const config: Config = {
   namespace: 'core',
   outputTargets: [
+    react({
+      componentCorePackage: '@you/ui-kit',
+      proxiesFile: '../react-library/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
---
+++ packages/react-library/src/components/stencil-generated/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@you/ui-kit';
+
+import { defineCustomElements } from '@you/ui-kit/loader';
+
+defineCustomElements();
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
